55a5dbb4478656b38d4c58181066f49b24f4e556
Amanda Elvkull <108291356+AmandaElvkull@users.noreply.github.com>
2022-11-18 20:42:17 +0100

    all test done

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,9 @@
 export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
-
-  if (type === 'string') {
-    return stringArrayOrObject === ''
+	const type = typeof stringArrayOrObject
+  
+	if (type === 'string') {
+	  return stringArrayOrObject === '' ? true : false
+	} else if( type === 'object' ) {
+	 return Object.keys(stringArrayOrObject).toString() === '' ? true : false
+	}
   }
-
-  return false
-}
